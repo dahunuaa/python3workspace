3cce4c6877e6f50dfe256d2ed79888f8e9c861e9
dahu yao <dahu yao>
2017-02-11 11:48:46 +0800

    反馈 通告模块增加编辑者姓名

--- baosteel100/apps/feedback/model.py
+++ baosteel100/apps/feedback/model.py
@@ -9,3 +9,13 @@ class FeedbackModel(model.StandCURDModel):
         ("contact",StrDT())
     ]
 
+    def __init__(self):
+        self.user_coll = model.BaseModel.get_model("user.UserModel").get_coll()
+        super(FeedbackModel,self).__init__()
+
+    def before_create(self,object):
+        user = self.user_coll.find_one({"_id":utils.create_objectid(object['add_user_id'])})
+        object['add_user_name'] = user['name']
+        self.coll.save(object)
+        return object
+
--- baosteel100/apps/notice/model.py
+++ baosteel100/apps/notice/model.py
@@ -9,3 +9,15 @@ class NoticeModel(model.StandCURDModel):
         ("notice_title",StrDT(required=True)),
         ("notice_text",StrDT(required=True))
     ]
+
+
+    def __init__(self):
+        self.user_coll = model.BaseModel.get_model("user.UserModel").get_coll()
+        super(NoticeModel, self).__init__()
+
+
+    def before_create(self,object):
+        user = self.user_coll.find_one({"_id": utils.create_objectid(object['add_user_id'])})
+        object['add_user_name'] = user['name']
+        self.coll.save(object)
+        return object
