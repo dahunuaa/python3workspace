813a034a39fcd92e62598dd1769f0282d2554717
dahu yao <dahu yao>
2017-02-25 14:56:09 +0800

    上传文件修改文件路径

--- baosteel100/apps/file/handler.py
+++ baosteel100/apps/file/handler.py
@@ -3,18 +3,22 @@ from baosteel100.apps.base.handler import TokenHandler,MultiStandardHandler,Sing
 from baosteel100.libs.oauthlib import get_provider
 import time
 import os
-import baosteel100.libs.utils as utils
-import traceback
+import baosteel100
 
+def get_root_path():
+    return os.path.dirname(os.path.abspath(baosteel100.__file__))
 
 class UploadFileHandler(MultiStandardHandler):
     _model = "file.FileModel"
 
     def post(self):
         #文件的暂存路径
-        upload_path = utils.get_root_path() +'/static/ftp/file/'
-        if not os.path.exists(os.path.dirname(upload_path)):
-            os.makedirs(os.path.dirname(upload_path))
+        host = self.request.host
+        relative_path =get_root_path() + '/static/ftp/file/'#文件存放的具体硬盘路径
+        upload_path = host +'/static/ftp/file/'
+
+        if not os.path.exists(os.path.dirname(relative_path)):
+            os.makedirs(os.path.dirname(relative_path))
         #提取表单中‘name’为‘file’的文件元数据
         file_metas=self.request.files['file']
         for meta in file_metas:
@@ -28,7 +32,8 @@ class UploadFileHandler(MultiStandardHandler):
             }
             self.coll.save(file)
             #有些文件需要已二进制的形式存储，实际中可以更改
-            with open(filepath,'wb') as up:
+            write_path = relative_path+filename
+            with open(write_path,'wb') as up:
                 up.write(meta['body'])
         self.result["data"] = {"file_name":filename,"file_path":filepath}
         self.finish(self.result)#此处用self.result['data']形式，回调函数是json类型，如果直接是result，回调函数的数据类型是text
