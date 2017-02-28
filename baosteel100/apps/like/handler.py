a5735eacb84b36670e9828702f13848b256dc7b1
dahu yao <dahu yao>
2017-02-28 19:26:25 +0800

    完成查看收藏消息详情

--- baosteel100/apps/like/handler.py
+++ baosteel100/apps/like/handler.py
@@ -2,6 +2,7 @@
 from baosteel100.apps.base.handler import MultiStandardHandler,SingleStandardHanler,TokenHandler
 from baosteel100.libs.oauthlib import get_provider
 
+
 class LikeHandler(MultiStandardHandler,TokenHandler):
     _model = "like.LikeModel"
     enable_methods = ["put","get"]
@@ -13,7 +14,21 @@ class LikeHandler(MultiStandardHandler,TokenHandler):
         res = self.model.alter(user_id,type,like_id)
         self.result["data"] = res
 
+class LikelistHandler(MultiStandardHandler,TokenHandler):
+    _model = "like.LikeModel"
+    enable_methods = ["get"]
+
+    def _get(self):
+        user_id = self.get_argument("user_id")
+        res = self.model.like_list(user_id)
+        self.result["data"]["like_coll"]=res[0]
+        self.result["data"]["buss_like_detail"]=res[1]
+        self.result["data"]["guide_like_detail"]=res[2]
+        self.result["data"]["gather_like_detail"]=res[3]
+
+
 handlers = [
-    (r"/add",LikeHandler,get_provider("like")),
-    (r"",LikeHandler,get_provider("file"))
+    (r"/alter",LikeHandler,get_provider("like")),
+    (r"/list",LikelistHandler,get_provider("like")),
+    (r"",LikeHandler,get_provider("like"))
 ]
--- baosteel100/apps/like/model.py
+++ baosteel100/apps/like/model.py
@@ -8,7 +8,10 @@ class LikeModel(model.StandCURDModel):
         ("user_id",StrDT(required=True)),
         ("buss_like",ListDT()),
         ("gather_like",ListDT()),
-        ("guide_like",ListDT())
+        ("guide_like",ListDT()),
+        ("buss_like_detail",ListDT()),
+        ("guide_like_detail",ListDT()),
+        ("gather_like_detail",ListDT())
     ]
 
     def __init__(self):
@@ -26,6 +29,7 @@ class LikeModel(model.StandCURDModel):
                     "buss_like":[],
                     "gather_like":[],
                     "guide_like":[],
+
                 }
                 self.coll.save(like)
 
@@ -48,3 +52,27 @@ class LikeModel(model.StandCURDModel):
                 _like["guide_like"].append(like_id)
         self.coll.save(_like)
         return utils.dump(_like)
+
+    def like_list(self,user_id):
+        buss_coll = model.BaseModel.get_model("business.BusinessModel").get_coll()
+        guide_coll = model.BaseModel.get_model("inforguide.InforguideModel").get_coll()
+        gather_coll = model.BaseModel.get_model("inforgather.InforgatherModel").get_coll()
+        like_coll=self.coll.find_one({"user_id":user_id})
+        guide_like_detail=[]
+        buss_like_detail=[]
+        gather_like_detail=[]
+        for i in like_coll["guide_like"]:
+            _guide=guide_coll.find_one({"_id":utils.create_objectid(i)})
+            guide_like_detail.append(_guide)
+        for j in like_coll["buss_like"]:
+            _buss = buss_coll.find_one({"_id":utils.create_objectid(j)})
+            buss_like_detail.append(_buss)
+        for k in like_coll["gather_like"]:
+            _gather = gather_coll.find_one({"_id":utils.create_objectid(k)})
+            gather_like_detail.append(_gather)
+
+        like_coll=utils.dump(like_coll)
+        buss_like_detail=utils.dump(buss_like_detail)
+        guide_like_detail=utils.dump(guide_like_detail)
+        gather_like_detail=utils.dump(gather_like_detail)
+        return like_coll,buss_like_detail,guide_like_detail,gather_like_detail
